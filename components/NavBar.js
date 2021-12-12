df7769d7c42b151566b618e057eb5014bb6b42d4
wailsamjouni <51680878+wailsamjouni@users.noreply.github.com>
2021-12-12 21:34:45 +0100

    initial commit

---
+++ components/NavBar.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import Link from 'next/link'
+import { FaMapMarkerAlt } from 'react-icons/fa'
+
+function NavBar() {
+    return (
+        <div className="flex flex-row space-x-60 px-10 py-7 shadow-xl">
+            <div className="flex justify-center items-center space-x-12">
+                <div>
+                    <a href='#'>
+                        <img src='https://cdn.usbrandcolors.com/images/logos/starbucks-logo.svg'
+                            className='w-12 h-12' />
+                    </a>
+                </div>
+                <div className='flex justify-center items-center space-x-6 font-bold text-sm tracking-widest'>
+                    <a href='#' className='hover:text-starbucks'>
+                        MENU
+                    </a>
+                    <a href='#' className='hover:text-starbucks'>
+                        REWARDS
+                    </a>
+                    <a href='#' className='hover:text-starbucks'>
+                        GIFT CARDS
+                    </a>
+                </div>
+            </div>
+            <div></div>
+            <div className='flex justify-center items-center space-x-7'>
+                <div className='flex justify-center items-center space-x-2  hover:cursor-pointer hover:text-starbucks
+                font-semibold text-sm'>
+                    <FaMapMarkerAlt size={'1.4rem'} />
+                    <h3>Find a store</h3>
+                </div>
+                <div className='flex justify-center items-center space-x-3 font-semibold text-sm'>
+                    <div className='flex px-4 py-1 rounded-2xl border border-black'>
+                        <button className=''>
+                            Sign in
+                        </button>
+                    </div>
+                    <div className='flex px-4 py-1 rounded-2xl border border-black'>
+                        <button className=''>
+                            Join now
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div >
+    )
+}
+
+export default NavBar
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      starbucks: '#00704A'
+    }
+  },
+  plugins: [],
+}
